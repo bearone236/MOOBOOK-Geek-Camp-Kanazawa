eb2cb9f2fffa01bb2584d6de392a32bbb53186f2
bearone236 <kuma_2306@icloud.com>
2023-07-27 23:13:01 +0900

    fix home.jsx

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Books from "./pages/books";
-import Book from "./pages/book";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Books from './pages/books';
+import Book from './pages/book';
+import Home from './pages/home';
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
+          <Route path="/home" element={<Home />} />
           <Route path="/" element={<Books />} />
           <Route path="/book/:id" element={<Book />} />
         </Routes>
